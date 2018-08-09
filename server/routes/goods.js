f5168ce92328d5577fbdb86d941a1ad5fb9bbd6f
revser0 <3300659338@qq.com>
2018-08-10 04:43:15 +0800

    商品列表分页服务端实现

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -18,34 +18,61 @@ mongoose.connection.on("disconnected", function(){
 });
 
 /* 查询商品列表数据 使用Goods */
+// router.get('/', function(req, res, next) {
+//   Goods.find(
+//     {},//条件查询
+//     {
+//       '_id':0,
+//       'productId': 1,
+//       'productName': 1,
+//       'salePrice': 1,
+//       'productImage': 1,
+//
+//     },//字段选择
+//     function(err, doc){
+//       if(err){
+//         res.json({
+//           status: '1',
+//           msg: err.message
+//         })
+//       }else {
+//         res.json({
+//           status: '0',
+//           msg:'',
+//           result:{
+//             count: doc.length,
+//             list: doc
+//           }
+//         })
+//       }
+//     });
+// });
 router.get('/', function(req, res, next) {
-  Goods.find(
-    {},//条件查询
-    {
-      '_id':0,
-      'productId': 1,
-      'productName': 1,
-      'salePrice': 1,
-      'productImage': 1,
-
-    },//字段选择
-    function(err, doc){
-      if(err){
-        res.json({
-          status: '1',
-          msg: err.message
-        })
-      }else {
-        res.json({
-          status: '0',
-          msg:'',
-          result:{
-            count: doc.length,
-            list: doc
-          }
-        })
-      }
-    });
+  let page = parseInt(req.param('page'));
+  let pageSize = parseInt(req.param('pageSize'));
+  let sort = req.param('sort');
+  let skip = (page-1)*pageSize;
+  let params = {};
+  //skip(), limilt(), sort()三个放在一起执行的时候，执行的顺序是先 sort(), 然后是 skip()，最后是显示的 limit()。
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+  goodsModel.sort({'salePrice': sort});
+  goodsModel.exec(function(err, doc) {
+    if(err){
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    }else {
+      res.json({
+        status: '0',
+        msg:'',
+        result:{
+          count: doc.length,
+          list: doc
+        }
+      })
+    }
+  });
 });
 
 
